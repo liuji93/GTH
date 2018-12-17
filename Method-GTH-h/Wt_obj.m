02edb073f1054be9b148e10ef49acfaca5a54a50
liuji93 <44867282+liuji93@users.noreply.github.com>
2018-12-17 22:09:12 +0800

    Delete Wt_obj.m

--- Method-GTH-h/Wt_obj.m
+++
@@ -1,7 +0,0 @@
-function [F,G] = Wt_obj(Wt,Ws,Xt,B,M,paras )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-F      = norm(M.*(Wt-Ws),'fro')^2+paras.lambda1*norm(B-Wt'*Xt)^2;
-G      = 2*M.*M.*(Wt-Ws)+2*paras.lambda1*(-Xt*B'+Xt*Xt'*Wt);
-end
-
