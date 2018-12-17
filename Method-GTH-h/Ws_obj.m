1d7b35d996588d6460a83db337fa25a5ee55a6a9
liuji93 <44867282+liuji93@users.noreply.github.com>
2018-12-17 22:09:01 +0800

    Delete Ws_obj.m

--- Method-GTH-h/Ws_obj.m
+++
@@ -1,5 +0,0 @@
-function [ F,G ] = Ws_obj(Ws,Wt,Xs,A,M,paras)
-F      = norm(M.*(Wt-Ws),'fro')^2+paras.lambda2*norm(A-Ws'*Xs,'fro')^2;
-G      = 2*M.*M.*(Ws-Wt)+2*paras.lambda2*(-Xs*A'+Xs*Xs'*Ws);
-end
-
