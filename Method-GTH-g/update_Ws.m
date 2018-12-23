cea7ecb74e487928a014507a678e947048bbd21d
liuji93 <44867282+liuji93@users.noreply.github.com>
2018-12-23 20:19:35 +0800

    Add files via upload

--- Method-GTH-g/update_Ws.m
+++ Method-GTH-g/update_Ws.m
@@ -3,15 +3,16 @@ function [ Ws] = update_Ws( Xs,Wt,Ws,A,paras)
 %   Detailed explanation goes here
 I  = eye(paras.d);
 %cont
-M      = I+paras.lambda2*Xs*Xs';
-N      = -2*Wt-2*paras.lambda2*Xs*A';
-G      = 2*M*Ws+N;
+G      = (2*I+paras.lambda2*Xs*Xs')*Ws-2*Wt-2*paras.lambda2*Xs*A';
 WG     = Ws'*G-G'*Ws;
 II     = eye(size(WG));
 WG     = (II+(paras.tao/2)*WG)\(II-(paras.tao/2)*WG);
 for t=1:5
      %WGs   = (II+(paras.tao/2)*WG)\(II-(paras.tao/2)*WG);
-     Ws   = Ws*WG;     
+     Ws   = Ws*WG;   
+     G    = (2*I+paras.lambda2*Xs*Xs')*Ws-2*Wt-2*paras.lambda2*Xs*A';
+     WG   = Ws'*G-G'*Ws;
+     WG   = (II+(paras.tao/2)*WG)\(II-(paras.tao/2)*WG);
 end
 end
 
--- Method-GTH-g/update_Wt.m
+++ Method-GTH-g/update_Wt.m
@@ -3,15 +3,16 @@ function [ Wt] = update_Wt(Xt,Wt,Ws,B,paras)
 %   Detailed explanation goes here
 I  = eye(paras.d);
 %cont
-M      = I+paras.lambda1*Xt*Xt';
-N      = -2*Ws-2*paras.lambda1*Xt*B';
-G      = 2*M*Wt + N;
+G      = (2*I+paras.lambda1*Xt*Xt')*Wt-2*Ws-2*paras.lambda1*Xt*B';
 WG     = Wt'*G-G'*Wt;
 II     = eye(size(WG));
 WG     = (II+(paras.tao/2)*WG)\(II-(paras.tao/2)*WG);
 for t=1:5
      %WGt  = (II+(paras.tao/2)*WG)\(II-(paras.tao/2)*WG);
-     Wt   = Wt*WG;    
+     Wt   = Wt*WG;   
+     G    = (2*I+paras.lambda1*Xt*Xt')*Wt-2*Ws-2*paras.lambda1*Xt*B';
+     WG   = Wt'*G-G'*Wt;
+     WG   = (II+(paras.tao/2)*WG)\(II-(paras.tao/2)*WG);
 end
 
 end
